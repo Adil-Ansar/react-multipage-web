f2df57173d5a9059c58b41f6d539dc6d2c495a1f
Adil-Ansar <ansari.adil2015@gmail.com>
2023-12-10 12:55:38 +0530

    Ok

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -9,19 +9,42 @@ export const GlobalStyle = createGlobalStyle`
  }
 html {
   font-size: 62.5%;
-//   scroll-behavior: smooth;
-//   1rem :10px
+  /* scroll-behavior: smooth; */
+  /* 1rem = 10px */
   overflow-x: hidden;
 }
 
+body {
+  overflow-x: hidden;
+   scrollbar-color: rgb(98 84 243);
+    scrollbar-width: thin;
+}
+
+body::-webkit-scrollbar {
+  width: 1.5rem;
+}
+
+body::-webkit-scrollbar-track {
+   background-color: rgb(24 24 29);
+}
+
+body::-webkit-scrollbar-thumb {
+ 
+  background: #fff;
+    border: 5px solid transparent;
+    border-radius: 9px;
+    background-clip: content-box;
+}
+
+
 h1{
-  color:${({theme}) => theme.colors.heading};
+  color:${({ theme }) => theme.colors.heading};
   font-size: 6rem;
   font-weight: 900;
 }
 
 h2 {
-   color: ${({theme}) => theme.colors.heading};
+   color: ${({ theme }) => theme.colors.heading};
    font-size: 4.4rem;
    font-weight: 300;
    white-space: normal;
@@ -34,7 +57,8 @@ h2 {
 }
 
 p {
-  color: ${({theme}) => theme.colors.text};
+  color: ${({ theme }) => theme.colors.text};
+  opacity: .7;
   font-size: 1.65rem;
   line-height: 1.5;
   margin-top: 1rem;
@@ -49,4 +73,25 @@ li {
   list-style: none;
 }
 
+.container {
+  max-width: 120rem;
+  margin: 0 auto;
+}
+
+.grid {
+  display: grid;
+  gap: 9rem;
+}
+
+.grid-two-column {
+  grid-template-columns: repeat(2, 1fr);
+}
+
+.grid-three-column {
+  grid-template-columns: repeat(3, 1fr);
+}
+
+.grid-four-column{
+   grid-template-columns: 1fr 1.2fr .5fr .8fr ;
+}
 `;
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button } from '../styles/Button';
+import { NavLink } from 'react-router-dom';
 
 const HeroSection = () => {
     return (
@@ -14,19 +15,70 @@ const HeroSection = () => {
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Et netus et malesuada fames ac turpis egestas.
                     </p>
+                    <Button className="btn hireme-btn">
+                        <NavLink to="/contact">hire me</NavLink>
+                    </Button>
                 </div>
-                <Button className="btn hireme-btn">
-                    <NavLink to="/contact">hire me</NavLink>
-                </Button>
                 <div className="section-hero-image">
-
+                    <picture>
+                        <img src="./images/hero.jpg" alt="hero img" className='hero-img'/>
+                    </picture>
                 </div>
             </div>
         </Wrapper>
-    )
-}
+    );
+};
 
-const Wrapper = styled.section` 
+const Wrapper = styled.section`
+  padding: 9rem 0;
+
+  .section-hero-data {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .btn {
+    max-width: 16rem;
+  }
+
+  .hero-top-data {
+    text-transform: uppercase;
+    font-weight: 500;
+    font-size: 1.5rem;
+    color: ${({ theme }) => theme.colors.helper};
+  }
+
+  .hero-heading {
+    text-transform: uppercase;
+    font-size: 6.4rem;
+  }
+
+  .hero-para {
+    margin-top: 1.5rem;
+    margin-bottom: 3.4rem;
+    max-width: 60rem;
+  }
+
+  .section-hero-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  picture {
+    text-align: center;
+  }
+
+  .hero-img {
+    max-width: 80%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid {
+      gap: 7.2rem;
+    }
+  }
 `;
 
 export default HeroSection;
